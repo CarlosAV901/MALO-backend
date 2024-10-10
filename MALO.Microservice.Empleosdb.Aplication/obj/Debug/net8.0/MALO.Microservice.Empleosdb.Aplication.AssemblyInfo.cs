20c16fc3a2c911c74ec0c64cf3392bfa231d2e27
GAma131 <gamaliel.g.g131@gmail.com>
2024-10-10 12:01:27 -0600

    Multimedia

--- MALO.Microservice.Empleosdb.Aplication/obj/Debug/net8.0/MALO.Microservice.Empleosdb.Aplication.AssemblyInfo.cs
+++ MALO.Microservice.Empleosdb.Aplication/obj/Debug/net8.0/MALO.Microservice.Empleosdb.Aplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MALO.Microservice.Empleosdb.Aplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f8e53ad35703a72446823b3c992c659daa95f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8519742e1bc9915bd0d8276d135a7ed28f36056")]
 [assembly: System.Reflection.AssemblyProductAttribute("MALO.Microservice.Empleosdb.Aplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MALO.Microservice.Empleosdb.Aplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
