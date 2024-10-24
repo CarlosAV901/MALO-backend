88d78cb2fbbd81f30ff6d3020e4a504e6c2ada4b
CHARLY\aguiv <aguivega123@hotmail.com>
2024-10-23 23:14:54 -0600

    Se corrigio error de DTO

--- MALO.Microservice.Empleosdb.Domain/DTOs/Empleos/EmpleoUpdateDto.cs
+++ MALO.Microservice.Empleosdb.Domain/DTOs/Empleos/EmpleoUpdateDto.cs
@@ -6,6 +6,8 @@ namespace MALO.Microservice.Empleosdb.Domain.DTOs.Empleos
         public Guid Empleo_id { get; set; }
         public string titulo { get; set; }
         public string descripcion { get; set; }
+        public Guid empresa_id { get; set; }
+        public DateTime fecha_publicacion { get; set; }
         public string ubicacion { get; set; }
         public decimal salario_minimo { get; set; }
         public decimal salario_maximo { get; set; }
