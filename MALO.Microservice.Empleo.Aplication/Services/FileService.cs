e58325d458305af8b0f05f94d052271be206dbff
CHARLY\aguiv <aguivega123@hotmail.com>
2024-11-07 12:24:43 -0600

    Se agrego la imagen de perfil para storage de firebase

---
+++ MALO.Microservice.Empleo.Aplication/Services/FileService.cs
@@ -0,0 +1,28 @@
+﻿using MALO.Microservice.Empleos.Domain.Interfaces.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace MALO.Microservice.Empleos.Aplication.Services
+{
+    public class FileService
+    {
+        private readonly IFilesHelper _filesHelper;
+
+        public FileService(IFilesHelper filesHelper)
+        {
+            _filesHelper = filesHelper;
+        }
+
+        public async Task<string> SubirArchivo(IFormFile archivo)
+        {
+            if (archivo == null || archivo.Length == 0)
+            {
+                throw new ArgumentException("El archivo no es válido.");
+            }
+
+            var nombre = archivo.FileName;
+            using var stream = archivo.OpenReadStream();
+            var urlImagen = await _filesHelper.SubirArchivo(stream, nombre);
+            return urlImagen;
+        }
+    }
+}
