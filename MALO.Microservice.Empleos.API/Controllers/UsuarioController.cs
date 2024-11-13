5ee04354a1a3fe8bf3dacead72e7def74d6dcaa9
CHARLY\aguiv <aguivega123@hotmail.com>
2024-11-12 19:37:07 -0600

    Se modifico el actualizar usuario

--- MALO.Microservice.Empleos.API/Controllers/UsuarioController.cs
+++ MALO.Microservice.Empleos.API/Controllers/UsuarioController.cs
@@ -208,7 +208,7 @@ namespace MALO.Microservice.Empleos.API.Controllers
 
 
 
-        [HttpGet("confirmar/{token}")]
+        [HttpGet("confirmar{token}")]
         public async Task<IActionResult> ConfirmarCorreo([FromQuery] Guid token)
         {
             
