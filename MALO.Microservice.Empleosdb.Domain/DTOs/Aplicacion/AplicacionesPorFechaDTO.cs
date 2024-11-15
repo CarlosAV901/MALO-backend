cafadb47cea8f8591f157d3b6c78fbce462185f4
CHARLY\aguiv <aguivega123@hotmail.com>
2024-11-15 12:37:46 -0600

    Se creo el enpoint para contar las aplicaciones por fecha

---
+++ MALO.Microservice.Empleosdb.Domain/DTOs/Aplicacion/AplicacionesPorFechaDTO.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MALO.Microservice.Empleosdb.Domain.DTOs.Aplicacion
+{
+    public class AplicacionesPorFechaDTO
+    {
+        public DateTime Fecha { get; set; }
+        public int TotalAplicaciones {  get; set; }
+    }
+}
--- MALO.Microservice.Empleosdb.Infraestructure/DataContexts/ManosALaObraContext.cs
+++ MALO.Microservice.Empleosdb.Infraestructure/DataContexts/ManosALaObraContext.cs
@@ -18,6 +18,7 @@ namespace MALO.Microservice.Empleosdb.Infraestructure.DataContexts
         public DbSet<ObtenerUsuariosPorEmpleosDTO> obtenerUsuariosPorEmpleosDTO {get; set;}
         public DbSet<AplicarEmpleoDTO> aplicarEmpleoDTO {get; set;}
         public DbSet<ObtenerEmpleosPorUsuarioDTO> obtenerEmpleosPorUsuarioDTO {get; set;}
+        public DbSet<AplicacionesPorFechaDTO> aplicacionesPorFechaDTO {get; set;}
 
         public DbSet<ActualizarMultimediaDTO> actualizarMultimediaDTO {get; set;}
         #endregion
@@ -32,6 +33,7 @@ namespace MALO.Microservice.Empleosdb.Infraestructure.DataContexts
             modelBuilder.Entity<ObtenerEmpleosPorUsuarioDTO>().HasNoKey();
             modelBuilder.Entity<UsuarioIdDTO>().HasNoKey();
             modelBuilder.Entity<AplicarEmpleoDTO>().HasNoKey();
+            modelBuilder.Entity<AplicacionesPorFechaDTO>().HasNoKey();
         }
 
 
